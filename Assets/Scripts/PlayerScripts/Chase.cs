7d655906ed87b7920e6c8582a10c52b436f9d337
mrobles7 <mrobles7@ucmerced.edu>
2017-06-22 16:59:21 -0700

    Update with navmesh, custom scripts, and running AI

---
+++ Assets/Scripts/PlayerScripts/Chase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chase : MonoBehaviour
+{
+    public Transform goal;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
